01036c81e7a9ef4861b070f048401ea3cb386d23
GeoTV777 <ivushka777@mail.ru>
2023-12-16 18:12:43 +0300

    Initial commit

---
+++ src/main/java/db/MySQLConnector.java
@@ -0,0 +1,71 @@
+package db;
+
+import settings.ISettings;
+import settings.PropertiesReader;
+
+import java.sql.*;
+import java.util.Map;
+
+public class MySQLConnector implements IDBConnector {
+
+    private static Connection connection = null;
+    private static Statement statement = null;
+    private void connect() {
+        ISettings reader = (ISettings) new PropertiesReader();
+
+        Map<String, String> settings = reader.read();
+        if (connection == null){
+            try {
+                connection = DriverManager
+                        .getConnection(settings.get("url") + "/" + settings.get("db_name"),
+                                settings.get("db_username"),
+                                settings.get("db_password"));
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (statement == null) {
+            try {
+                statement = connection.createStatement();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void executeRequest(String response) {
+        try {
+            statement.execute(response);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ResultSet executeRequestWithAnswer(String response) {
+        try {
+            return statement.executeQuery(response);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void close() {
+        if (statement != null) {
+            try {
+                statement.close();
+                statement = null;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (connection != null) {
+            try {
+                connection.close();
+                connection = null;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
